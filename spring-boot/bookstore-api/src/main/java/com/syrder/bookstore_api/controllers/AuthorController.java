4ffb1d80add66461fae8cefad53c8b5d7c7f440d
SyrderBaptichon <dorybaptichon@gmail.com>
2025-09-20 13:40:04 +0200

    Create Athor List Endpoint

--- spring-boot/bookstore-api/src/main/java/com/syrder/bookstore_api/controllers/AuthorController.java
+++ spring-boot/bookstore-api/src/main/java/com/syrder/bookstore_api/controllers/AuthorController.java
@@ -6,9 +6,13 @@ import com.syrder.bookstore_api.mappers.Mapper;
 import com.syrder.bookstore_api.services.AuthorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AuthorController {
@@ -27,5 +31,13 @@ public class AuthorController {
         AuthorEntity authorEntity = authorMapper.mapFrom(author);
         AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
         return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/authors")
+    public List<AuthorDto> listAuthors() {
+        List<AuthorEntity> authors = authorService.findAll();
+        return authors.stream()
+                .map(authorMapper::mapTo)
+                .collect(Collectors.toList());
     }
 }
