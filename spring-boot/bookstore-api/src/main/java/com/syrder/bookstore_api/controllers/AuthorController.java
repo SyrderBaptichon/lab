1527a81ffec52d4ca4fb125322b480bf1558807a
SyrderBaptichon <dorybaptichon@gmail.com>
2025-09-20 10:47:03 +0200

    Test Author Create Endpoint

--- spring-boot/bookstore-api/src/main/java/com/syrder/bookstore_api/controllers/AuthorController.java
+++ spring-boot/bookstore-api/src/main/java/com/syrder/bookstore_api/controllers/AuthorController.java
@@ -4,6 +4,8 @@ import com.syrder.bookstore_api.domain.dto.AuthorDto;
 import com.syrder.bookstore_api.domain.entities.AuthorEntity;
 import com.syrder.bookstore_api.mappers.Mapper;
 import com.syrder.bookstore_api.services.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +23,9 @@ public class AuthorController {
     }
 
     @PostMapping(path = "/authors")
-    public AuthorDto createAuthor(@RequestBody AuthorDto author) {
+    public ResponseEntity<AuthorDto> createAuthor(@RequestBody AuthorDto author) {
         AuthorEntity authorEntity = authorMapper.mapFrom(author);
         AuthorEntity savedAuthorEntity = authorService.createAuthor(authorEntity);
-        return authorMapper.mapTo(savedAuthorEntity);
+        return new ResponseEntity<>(authorMapper.mapTo(savedAuthorEntity), HttpStatus.CREATED);
     }
 }
---
+++ spring-boot/bookstore-api/src/test/java/com/syrder/bookstore_api/controllers/BookControllerIntegrationTests.java
@@ -0,0 +1,4 @@
+package com.syrder.bookstore_api.controllers;
+
+public class BookControllerIntegrationTests {
+}
